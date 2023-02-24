31a8bc25ce80daea2ff29e57ecadde6df6f94158
choh <ctndefru@columbusairports.com>
2023-02-24 17:21:08 -0500

    G C

--- Git/terraform/backend.tf
+++ Git/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "TF-AzDo-Demo-RG"
     storage_account_name = "tfazdodemostg0102"
     container_name       = "terraform-state"
-    key                  = "tf-azdo-demo0150.tfstate"
+    key                  = "tf-azdo-demo0151.tfstate"
 
   }
 }
