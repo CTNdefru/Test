1975dcf87880c0435d3422c2678cdcb3ec5927f3
choh <ctndefru@columbusairports.com>
2023-02-25 06:37:28 -0500

    1file

--- Git/vm/main.tf
+++ Git/vm/main.tf
@@ -5,3 +5,11 @@ resource "azurerm_resource_group" "craa-rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_resource_group" "craa-rg" {
+  name     = "Help-rg"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
+}
